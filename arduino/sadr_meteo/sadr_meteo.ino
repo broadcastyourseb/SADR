c81d97c6b36679ec8a79e3ca8d4d2c7fb1acd6e3
broadcastyourseb <sebastien.leclerc.1@gmail.com>
2017-07-24 07:16:07 +0200

    new firmware arduino and makefile

--- arduino/sadr_meteo/sadr_meteo.ino
+++ arduino/sadr_meteo/sadr_meteo.ino
@@ -15,18 +15,18 @@ IMPORTANT: Customize following values to match your setup
 */
 
 //Comment out if you setup don't include some sensor.
-//#define USE_DHT_SENSOR_INTERNAL   //USE INTERNAL DHT HUMITITY SENSOR. Comment if not.
+#define USE_DHT_SENSOR_INTERNAL   //USE INTERNAL DHT HUMITITY SENSOR. Comment if not.
 //#define USE_DHT_SENSOR_EXTERNAL   //USE EXTERNAL DHT HUMITITY SENSOR. Comment if not.
-#define USE_IR_SENSOR   //USE MELEXIS IR SENSOR. Comment if not.
-#define USE_P_SENSOR   //USE BME280 PRESSURE SENSOR. Comment if not.
-#define USE_WIND_SENSOR   //USE SWITCH ANEMOMETER. Comment if not.
-#define USE_LIGHT_SENSOR   //USE SOLAR PANEL AS LIGHT SENSOR. Comment if not.
+//#define USE_IR_SENSOR   //USE MELEXIS IR SENSOR. Comment if not.
+//#define USE_P_SENSOR   //USE BME280 PRESSURE SENSOR. Comment if not.
+//#define USE_WIND_SENSOR   //USE ANEMOMETER type SWITCH. Comment if not.
+//#define USE_LIGHT_SENSOR   //USE SOLAR PANEL AS LIGHT SENSOR. Comment if not.
 //#define USE_DHT_RAIN_SENSOR   //USE TELECONTROLLI CAPACITIVE RAIN SENSOR. Comment if not.
 
-//All sensors (ThrI=DHT22 INT,ThrE=DHT22 EXT,Tir=MELEXIS and Tp=BME280) include a ambient temperature
+//All sensors (T22int=DHT22 INT,T22ext=DHT22 EXT,Tir=MELEXIS and Tp=BME280) include a ambient temperature
 //Choose  that sensor, only one, is going to use for main Ambient Temperature:
-#define T_MAIN_ThrE
-//#define T_MAIN_ThrI
+//#define T_MAIN_T22ext
+#define T_MAIN_T22int
 //#define T_MAIN_Tir  
 //#define T_MAIN_Tp
 
@@ -54,7 +54,10 @@ IMPORTANT: Customize following values to match your setup
 // Activation threshold for daylight (between 0 and 1024)
 #define DAYLIGHT_FLAG_TRIGGER 5
 
-
+// Activation threshold for wind in km/h
+#define WIND_FLAG_TRIGGER 10
+// Rotation count period to estimate the wind speed in millisecond
+#define WIND_MEAN_TIME 3000
 
 /*END OFF CUSTOMIZATION. YOU SHOULD NOT NEED TO CHANGE ANYTHING BELOW */
 
@@ -126,15 +129,17 @@ IMPORTANT: Customize following values to match your setup
   #include <Adafruit_MLX90614.h>
 #endif //USE_IR_SENSOR
 
-#ifdef USE_DHT_SENSOR_INTERNAL
+#if defined(USE_DHT_SENSOR_INTERNAL) || defined(USE_DHT_SENSOR_EXTERNAL)
   #include "DHT.h"
+#endif //USE_DHT_SENSOR_INTERNAL
+
+#ifdef USE_DHT_SENSOR_INTERNAL
   #define DHT_INT_PIN 2         // what pin we're connected DHT22 to
   #define DHT_INT_TYPE DHT22   // DHT 22  (AM2302), AM2321
   DHT dhtInt(DHT_INT_PIN, DHT_INT_TYPE);
 #endif //USE_DHT_SENSOR_INTERNAL
 
 #ifdef USE_DHT_SENSOR_EXTERNAL
-  #include "DHT.h"
   #define DHT_EXT_PIN 4         // what pin we're connected DHT22 to
   #define DHT_EXT_TYPE DHT22   // DHT 22  (AM2302), AM2321
   DHT dhtExt(DHT_EXT_PIN, DHT_EXT_TYPE);
@@ -155,16 +160,81 @@ IMPORTANT: Customize following values to match your setup
 
 #ifdef USE_WIND_SENSOR
    #include <math.h>
+   #define  ANEMOMETER_PIN 3    // the pin that the pushbutton is attached to
 #endif //USE_WIND_SENSOR*/
 
-/*float HR,Thr,P,IR,T,Tp,Tir,Dew,Light,Clouds,skyT;*/
-float T22int,Hr22int,DewInt,T22ext,Hr22ext,DewExt,Light,Tp,P;
-float T,IR,Clouds,skyT,Tir;
+float T22int,Hr22int,DewInt,T22ext,Hr22ext,DewExt,Light,Tp,P,T,IR,Clouds,skyT,Tir,WindSpeed;
 int cloudy,dewing,frezzing,windy,rainy,daylight;
+volatile unsigned long Rotations; // cup rotation counter used in interrupt routine
+volatile unsigned long ContactBounceTime; // Timer to avoid contact bounce in interrupt routine
 
-#define TOTAL_ANALOG_PINS       11
-#define TOTAL_PINS              25
+#define TOTAL_ANALOG_PINS       15
+#define TOTAL_PINS              29
 
+#if defined(USE_DHT_SENSOR_INTERNAL) || defined(USE_DHT_SENSOR_EXTERNAL)
+// dewPoint function NOAA
+// reference: http://wahiduddin.net/calc/density_algorithms.htm 
+double dewPoint(double celsius, double humidity)
+{
+        double A0= 373.15/(273.15 + celsius);
+        double SUM = -7.90298 * (A0-1);
+        SUM += 5.02808 * log10(A0);
+        SUM += -1.3816e-7 * (pow(10, (11.344*(1-1/A0)))-1) ;
+        SUM += 8.1328e-3 * (pow(10,(-3.49149*(A0-1)))-1) ;
+        SUM += log10(1013.246);
+        double VP = pow(10, SUM-3) * humidity;
+        double T = log(VP/0.61078);   // temp var
+        return (241.88 * T) / (17.558-T);
+}
+
+// delta max = 0.6544 wrt dewPoint()
+// 5x faster than dewPoint()
+// reference: http://en.wikipedia.org/wiki/Dew_point
+double dewPointFast(double celsius, double humidity)
+{
+        double a = 17.271;
+        double b = 237.7;
+        double temp = (a * celsius) / (b + celsius) + log(humidity/100);
+        double Td = (b * temp) / (a - temp);
+        return Td;
+}
+#endif //USE_DHT_SENSOR_INTERNAL || USE_DHT_SENSOR_EXTERNAL
+
+#ifdef USE_IR_SENSOR
+//From AAG Cloudwatcher formula. Need to improve futher.
+//http://www.aagware.eu/aag/cloudwatcher700/WebHelp/index.htm#page=Operational%20Aspects/23-TemperatureFactor-.htm
+//https://azug.minpet.unibas.ch/wikiobsvermes/index.php/AAG_cloud_sensor#Snow_on_the_sky_temperature_sensor
+double skyTemp() {
+  //Constant defined above
+  double Td = (K1 / 100.) * (T - K2 / 10) + (K3 / 100.) * pow((exp (K4 / 1000.* T)) , (K5 / 100.));
+  double Tsky=IR-Td;
+  return Tsky;
+}
+
+
+double cloudIndex() {
+   double Tcloudy= CLOUD_TEMP_OVERCAST,Tclear=CLOUD_TEMP_CLEAR;
+   double Tsky=skyTemp();
+   double Index;
+   if (Tsky<Tclear) Tsky=Tclear;
+   if (Tsky>Tcloudy) Tsky=Tcloudy;
+   Index=(Tsky-Tclear)*100/(Tcloudy-Tclear);
+   return Index;
+}
+#endif //USE_IR_SENSOR
+
+#ifdef USE_WIND_SENSOR
+// This is the function that the interrupt calls to increment the rotation count
+void isr_rotation () {
+
+  if ((millis() - ContactBounceTime) > 15 ) { // debounce the switch contact.
+    Rotations++;
+    ContactBounceTime = millis();
+  }
+}
+#endif //USE_WIND_SENSOR
+
+// This is the function called to initialize each activate sensor
 void setupMeteoStation(){
 
 #ifdef USE_P_SENSOR
@@ -182,6 +252,13 @@ void setupMeteoStation(){
 #ifdef USE_IR_SENSOR
    mlx.begin();
 #endif //USE_IR_SENSOR*/
+
+#ifdef USE_WIND_SENSOR
+  // initialize the anemometer as a input:
+  pinMode(ANEMOMETER_PIN, INPUT);
+  // interrupt when switch contact is closed
+  attachInterrupt(digitalPinToInterrupt(ANEMOMETER_PIN), isr_rotation, FALLING);
+#endif //USE_WIND_SENSOR*/
 
 }
 
@@ -264,21 +341,41 @@ void runMeteoStation() {
     }
 #endif //USE_LIGHT_SENSOR
 
-#if defined T_MAIN_Thre
-    T=Thre;
-#elif defined T_MAIN_Thri
-    T=Thri;
+#if defined T_MAIN_T22ext
+    T=T22ext;
+#elif defined T_MAIN_T22int
+    T=T22int;
 #elif defined T_MAIN_Tir  
     T=Tir;
 #elif defined T_MAIN_Tp
     T=Tp;
 #endif  //T_MAIN  
 
-    if (T <=2) {
-      frezzing=1;
+if (T <=2) {
+  frezzing=1;
+} else {
+  frezzing=0;
+}
+
+#ifdef USE_WIND_SENSOR
+  Rotations = 0; // Set Rotations count to 0 ready for calculations
+  
+  sei(); // Enables interrupts
+  delay (WIND_MEAN_TIME); // Wait x seconds to average
+  cli(); // Disable interrupts
+  
+  // A wind speed of 1.492 MPH (2.4 km/h) causes the switch to close once per second. 
+  // V = R / 3 * 2.4 = R * 0.8
+  
+  WindSpeed = Rotations * 0.75;
+  
+  if (WindSpeed > WIND_FLAG_TRIGGER) {
+        windy=1;
     } else {
-      frezzing=0;
+        windy=0;
     }
+#endif //USE_WIND_SENSOR*/
+
 }
 
 void checkMeteo() {
@@ -295,81 +392,29 @@ void checkMeteo() {
        digitalWrite(PIN_TO_DIGITAL(6), LOW); // disable internal pull-ups 
     }
 
-   if (frezzing == 1) {
+   if (frezzing==1) {
        digitalWrite(PIN_TO_DIGITAL(7), HIGH); // enable internal pull-ups 
     } else {
        digitalWrite(PIN_TO_DIGITAL(7), LOW); // disable internal pull-ups 
     }   
   
-    if (daylight == 1) {
+    if (daylight==1) {
        digitalWrite(PIN_TO_DIGITAL(8), HIGH); // enable internal pull-ups 
     } else {
        digitalWrite(PIN_TO_DIGITAL(8), LOW); // disable internal pull-ups  
     }
-    if (windy>10) {
+    if (windy==1) {
        digitalWrite(PIN_TO_DIGITAL(9), HIGH); // enable internal pull-ups 
     } else {
        digitalWrite(PIN_TO_DIGITAL(9), LOW); // disable internal pull-ups  
     }  
-    if (rainy>150) {
+    if (rainy==1) {
        digitalWrite(PIN_TO_DIGITAL(10), HIGH); // enable internal pull-ups 
     } else {
        digitalWrite(PIN_TO_DIGITAL(10), LOW); // disable internal pull-ups  
     }   
 
 }
-
-#if defined(USE_DHT_SENSOR_INTERNAL) || defined(USE_DHT_SENSOR_EXTERNAL)
-// dewPoint function NOAA
-// reference: http://wahiduddin.net/calc/density_algorithms.htm 
-double dewPoint(double celsius, double humidity)
-{
-        double A0= 373.15/(273.15 + celsius);
-        double SUM = -7.90298 * (A0-1);
-        SUM += 5.02808 * log10(A0);
-        SUM += -1.3816e-7 * (pow(10, (11.344*(1-1/A0)))-1) ;
-        SUM += 8.1328e-3 * (pow(10,(-3.49149*(A0-1)))-1) ;
-        SUM += log10(1013.246);
-        double VP = pow(10, SUM-3) * humidity;
-        double T = log(VP/0.61078);   // temp var
-        return (241.88 * T) / (17.558-T);
-}
-
-// delta max = 0.6544 wrt dewPoint()
-// 5x faster than dewPoint()
-// reference: http://en.wikipedia.org/wiki/Dew_point
-double dewPointFast(double celsius, double humidity)
-{
-        double a = 17.271;
-        double b = 237.7;
-        double temp = (a * celsius) / (b + celsius) + log(humidity/100);
-        double Td = (b * temp) / (a - temp);
-        return Td;
-}
-#endif //USE_DHT_SENSOR_INTERNAL || USE_DHT_SENSOR_EXTERNAL
-
-#ifdef USE_IR_SENSOR
-//From AAG Cloudwatcher formula. Need to improve futher.
-//http://www.aagware.eu/aag/cloudwatcher700/WebHelp/index.htm#page=Operational%20Aspects/23-TemperatureFactor-.htm
-//https://azug.minpet.unibas.ch/wikiobsvermes/index.php/AAG_cloud_sensor#Snow_on_the_sky_temperature_sensor
-double skyTemp() {
-  //Constant defined above
-  double Td = (K1 / 100.) * (T - K2 / 10) + (K3 / 100.) * pow((exp (K4 / 1000.* T)) , (K5 / 100.));
-  double Tsky=IR-Td;
-  return Tsky;
-}
-
-
-double cloudIndex() {
-   double Tcloudy= CLOUD_TEMP_OVERCAST,Tclear=CLOUD_TEMP_CLEAR;
-   double Tsky=skyTemp();
-   double Index;
-   if (Tsky<Tclear) Tsky=Tclear;
-   if (Tsky>Tcloudy) Tsky=Tcloudy;
-   Index=(Tsky-Tclear)*100/(Tcloudy-Tclear);
-   return Index;
-}
-#endif //USE_IR_SENSOR
 
 /* Nacho Mas. 
 	Change the value returned by readAnalog before send through
@@ -419,7 +464,18 @@ int mapAndSendAnalog(int pin) {
        case 10:     
                result=(T+273)*20;
                break;   
-               
+       case 11:     
+               result=WindSpeed;
+               break;
+       case 12:     
+               result=Hr22int*100;
+               break;       
+       case 13:     
+               result=(T22int+273)*20;
+               break;                      
+       case 14:     
+               result=(DewInt+273)*20;
+               break;        
                
        default:
              result=value;
