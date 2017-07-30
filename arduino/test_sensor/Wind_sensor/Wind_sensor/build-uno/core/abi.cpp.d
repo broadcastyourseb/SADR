16616017b160ab1c2ce1717311958b4466f95de2
broadcastyourseb <sebastien.leclerc.1@gmail.com>
2017-07-30 18:03:04 -0400

    all Makefile working on raspberry

--- arduino/test_sensor/Wind_sensor/Wind_sensor/build-uno/core/abi.cpp.d
+++
@@ -1,2 +0,0 @@
-build-uno/core/abi.cpp.o: \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/cores/arduino/abi.cpp
--- arduino/test_sensor/Wind_sensor/Wind_sensor/build-uno/platformlibs/Wire/src/utility/twi.c.d
+++
@@ -1,6 +0,0 @@
-build-uno/platformlibs/Wire/src/utility/twi.c.o: \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/libraries/Wire/src/utility/twi.c \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/cores/arduino/Arduino.h \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/cores/arduino/binary.h \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/variants/standard/pins_arduino.h \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/libraries/Wire/src/utility/twi.h
