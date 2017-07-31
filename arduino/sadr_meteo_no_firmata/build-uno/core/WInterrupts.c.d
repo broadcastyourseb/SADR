14c0a778ed5a86a086249a33a70b339792b49be9
broadcastyourseb <sebastien.leclerc.1@gmail.com>
2017-07-31 16:40:34 -0400

    arduino firmware modify to adapt the output sentence with :

--- arduino/sadr_meteo_no_firmata/build-uno/core/WInterrupts.c.d
+++
@@ -1,6 +0,0 @@
-build-uno/core/WInterrupts.c.o: \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/cores/arduino/WInterrupts.c \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/cores/arduino/wiring_private.h \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/cores/arduino/Arduino.h \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/cores/arduino/binary.h \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/variants/standard/pins_arduino.h
--- arduino/sadr_meteo_no_firmata/build-uno/platformlibs/Wire/src/Wire.cpp.d
+++
@@ -1,8 +0,0 @@
-build-uno/platformlibs/Wire/src/Wire.cpp.o: \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/libraries/Wire/src/Wire.cpp \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/libraries/Wire/src/utility/twi.h \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/libraries/Wire/src/Wire.h \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/cores/arduino/Stream.h \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/cores/arduino/Print.h \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/cores/arduino/WString.h \
- /home/pi/arduino-1.8.3/hardware/arduino/avr/cores/arduino/Printable.h
