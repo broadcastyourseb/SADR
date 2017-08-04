5047f131de950cb61b0eb946a9608d880dfdff47
broadcastyourseb <sebastien.leclerc.1@gmail.com>
2017-08-05 00:56:47 +0200

    adding json stuff

--- raspberry/allskySCRIPT/meteoRRD_updater.py
+++ raspberry/allskySCRIPT/meteoRRD_updater.py
@@ -59,12 +59,24 @@ fi.close()
 while (True):
   try:
     now=time.localtime()
+    json_dict={"TIME":time.strftime("%c",now)}
     data=recv_serial()
+    splitData = data.split(':')
+    tupleData = (("T",splitData[1]), ("frezzing",splitData[2]), ("Light",splitData[3]), ("daylight",splitData[4]), ("T22int",splitData[5]), ("Hr22int",splitData[6]), ("T22ext",splitData[7]), ("Hr22ext",splitData[8]), ("DewExt",splitData[9]), ("dewing",splitData[10]), ("Tir",splitData[11]), ("IR",splitData[12]), ("skyT",splitData[13]), ("Clouds",splitData[14]), ("cloudy",splitData[15]), ("P",splitData[16]), ("Tp",splitData[17]), ("WindSpeed",splitData[18]), ("MaxWindSpeed",splitData[19]), ("windy",splitData[20]), ("Capacity",splitData[21]), ("rainy",splitData[22]), ("Consigne",splitData[23]), ("Temp",splitData[24]), ("Mosfet",splitData[25]))
+    for d in tupleData:
+         #print d[0],d[1]
+         json_dict[d[0]]=int(d[1]*100)/100.
     print(data)
     ret = rrdtool.update(CHARTPATH+'meteo.rrd',data);
     if ret:
-        print rrdtool.error() 
+        print rrdtool.error()
+        x = simplejson.dumps(json_dict)
+    #print x
+    fi=open(CHARTPATH+"RTdata.json","w")
+	fi.write(x)
+	fi.close()
     del data
+    del json_dict
     collected = gc.collect()
     time.sleep(30)
   except:
