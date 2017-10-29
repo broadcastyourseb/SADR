c83fdc3c6fbe069a38b812b240d1bb575af23012
broadcastyourseb <sebastien.leclerc.1@gmail.com>
2017-10-29 23:34:31 +0100

    change wind sensor firmware test

--- arduino/test_sensor/Wind_sensor/Wind_sensor/Wind_sensor.ino
+++ arduino/test_sensor/Wind_sensor/Wind_sensor/Wind_sensor.ino
@@ -1,31 +1,67 @@
-#include <math.h>
+float WindSpeed,MaxWindSpeed,MiniTime,reedSwitchTime;
+volatile unsigned long reedSwitchCount; // cup rotation counter used in interrupt routine
+volatile unsigned long ContactBounceTime; // Timer to avoid contact bounce in interrupt routine
+unsigned long tempoSerial, TimeStamp;
 
-// this constant won't change:
-const int  WindSensorPin = 3;    // the pin that the pushbutton is attached to
-const int ledPin = 13;       // the pin that the LED is attached to
+// Delay between 2 serial printing
+#define SERIAL_DELAY 10000
 
-int Rotations; // cup rotation counter used in interrupt routine
-unsigned long ContactBounceTime; // Timer to avoid contact bounce in interrupt routine
+//Comment out if you setup don't include some sensor.
+#define USE_WIND_SENSOR   //USE ANEMOMETER type SWITCH. Comment if not.
 
-double WindSpeed; // speed miles per hour
+// Wind sensor
+// Activation threshold for wind in km/h
+#define WIND_FLAG_TRIGGER 20
+// A wind speed of 1.492 MPH (2.4 km/h) causes the switch to close once per second. 
+#define WIND_RPM_TO_KMH 25 // Relation between windspeed in km/h and rotation per minute
+#define ANEMOMETER_BOUNCE_TIME 5 //Below this value, no count is adding
+
+#ifdef USE_WIND_SENSOR
+  #include <math.h>
+  #define  ANEMOMETER_PIN 3    // the pin that the pushbutton is attached to
+#endif //USE_WIND_SENSOR*/
 
 // This is the function that the interrupt calls to increment the rotation count
+#ifdef USE_WIND_SENSOR
+// This is the function that the interrupt calls to increment the rotation count
 void isr_rotation () {
-
-  if ((millis() - ContactBounceTime) > 15 ) { // debounce the switch contact.
-    Rotations++;
-    ContactBounceTime = millis();
+  TimeStamp = millis();
+  if ((TimeStamp - ContactBounceTime) > ANEMOMETER_BOUNCE_TIME ) { // debounce the switch contact.
+    reedSwitchCount++;
+    if (reedSwitchCount > 1) {
+        reedSwitchTime += TimeStamp - ContactBounceTime;
+    }
+    if ((TimeStamp - ContactBounceTime) < MiniTime) {
+        MiniTime = TimeStamp - ContactBounceTime;
+    }
   }
+  ContactBounceTime = TimeStamp ;
+  // Winspeed and MaxWindSpeed in RPM - switch closed 2 times per revolution - SERIAL_DELAY in ms
+  //WindSpeed = (reedSwitchCount / 2 * 60) / (reedSwitchTime / 1000);
+  WindSpeed = (reedSwitchCount - 1) / reedSwitchTime * 30000;
+  if (MiniTime == SERIAL_DELAY || MiniTime == 0) {
+    MaxWindSpeed = WindSpeed ;
+  } else {
+    //MaxWindSpeed = (1 / 2 * 60) / (MiniTime / 1000);
+    MaxWindSpeed = 30000 / MiniTime;
+  }
+  // Winspeed and MaxWindSpeed in Km/h
+  WindSpeed = WindSpeed / WIND_RPM_TO_KMH;
+  MaxWindSpeed = MaxWindSpeed / WIND_RPM_TO_KMH;
 }
+
+#endif //USE_WIND_SENSOR
 
 void setup() {
 
+#ifdef USE_WIND_SENSOR
   // initialize the anemometer as a input:
-  pinMode(WindSensorPin, INPUT);
-    // initialize the LED as an output:
-  pinMode(ledPin, OUTPUT);
+  pinMode(ANEMOMETER_PIN, INPUT);
+  // interrupt when switch contact is closed
+  attachInterrupt(digitalPinToInterrupt(ANEMOMETER_PIN), isr_rotation, FALLING);
+#endif //USE_WIND_SENSOR*/
 
-  attachInterrupt(digitalPinToInterrupt(WindSensorPin), isr_rotation, FALLING);
+
 
   // initialize serial communication:
   Serial.begin(9600);
@@ -34,17 +70,19 @@ void setup() {
 
 void loop() {
 
-  Rotations = 0; // Set Rotations count to 0 ready for calculations
-  
-  sei(); // Enables interrupts
-  delay (1000); // Wait 1 seconds to average
-  cli(); // Disable interrupts
-  
-  // A wind speed of 1.492 MPH (2.4 km/h) causes the switch to close once per second. 
-  // V = R / 30 * 2.4 = R * 0.8
-  
-  WindSpeed = Rotations * 0.8;
-  
-  Serial.print(Rotations); Serial.print("\t\t");
-  Serial.println(WindSpeed);
+  if ((millis() - tempoSerial) > SERIAL_DELAY ) { // time between 2 serial sending.
+
+    Serial.print(WindSpeed);
+    Serial.print(":");
+    Serial.print(MaxWindSpeed);
+    Serial.print(":");
+    
+    tempoSerial = millis();
+    WindSpeed = 0; // Set WindSpeed count to 0 after calculations
+    MaxWindSpeed = 0; // Set MaxWindSpeed count to 0 after calculations
+    ContactBounceTime = 0; // Set ContactBounceTime count to 0 after calculations
+    reedSwitchCount = 0; // Set reedSwitchCount count to 0 after calculations
+    reedSwitchTime = 0; // Set reedSwitchTime to 0 afer calculations
+    MiniTime = SERIAL_DELAY; // Set MiniTime value to SERIAL_DELAY after calculations
+  }
 }
